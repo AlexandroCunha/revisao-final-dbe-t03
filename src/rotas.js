dcf66dfcd5c1246d5c82e8551187b1da8e24c3ec
alexandro cunha <alexandrocunha4@gmail.com>
2023-12-08 23:38:30 -0300

    go

--- src/rotas.js
+++ src/rotas.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const { logarUsuario } = require("./controladores/usuarios");
-const validarCorpoRequisicao = require("./intermediarios/validarCorpoRequisicao");
-const { usuariosEsquemaLogin } = require("./esquemas/usuarios");
-const { criarReceita, salvarReceita } = require("./controladores/receitas");
-const verificarToken = require("./intermediarios/varificarToken");
-const rotas = express();
+const express = require("express")
+const { logarUsuario } = require("./controladores/usuarios")
+const validarCorpoRequisicao = require("./intermediarios/validarCorpoRequisicao")
+const { usuariosEsquemaLogin } = require("./esquemas/usuarios")
+const { criarReceita, salvarReceita } = require("./controladores/receitas")
+const verificarToken = require("./intermediarios/varificarToken")
+const rotas = express()
 
-rotas.get("/", (_req, res) => res.send("Olá, mundo!"));
+rotas.get("/", (_req, res) => res.send("Olá, mundo!"))
 rotas.post(
   "/login",
   validarCorpoRequisicao(usuariosEsquemaLogin),
   logarUsuario
 );
 
-rotas.use(verificarToken);
+rotas.use(verificarToken)
 
-rotas.post("/receitas", criarReceita);
-rotas.post("/salvar-receitas", salvarReceita);
+rotas.post("/receitas", criarReceita)
+rotas.post("/salvar-receitas", salvarReceita)
 
-module.exports = rotas;
+module.exports = rotas
